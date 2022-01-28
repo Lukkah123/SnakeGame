ca8b99b82761a5dc1d854e58c7a55b09e44a4e1a
Lukkah123 <h_lukka@hotmail.com>
2022-01-28 09:23:15 +0100

    added food and bodyparts, so that the snake increases in size when eating. Also added initial scoredisplay but still doesnt add the count to it

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
+let canvas = document.getElementById('game');
+let ctx = canvas.getContext('2d');
+
+class SnakeBody{
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 let speed = 7;
 let tileCount = 20;
 let tileSize = 18;
 let headX = 10;
 let headY = 10;
+let snakeBodyParts = [];
+let tailLength = 1;
+
+let foodX = 5;
+let foodY = 5;
 
 //variables to move the snake around
 let xMomentum = 0;
 let yMomentum = 0;
 
+let score = 0;
+
 //looping the game depending on the speed variable
 function updateGame() {
   clearScreen();
   changeSnakePosition();
+  checkFoodCollision();
+  getFood();
   getSnake();
+  getScore();
   setTimeout(updateGame, 1000 / speed);
+}
+
+function getScore() {
+  ctx.fillStyle = 'white';
+  ctx.font = '10px Arial';
+  ctx.fillText("Score: " + score,10, 390);
 }
 
 function clearScreen() {
@@ -26,14 +49,43 @@ function clearScreen() {
 
 function getSnake() {
   ctx.fillStyle = 'green';
+  //tilesize is a little bit smaller just to have a small gap
   ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+
+  ctx.fillStyle = 'green';
+  for (let i = 0; i < snakeBodyParts.length; i++){
+    let part = snakeBodyParts[i];
+    ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  }
+
+  //put a part in the end of the list
+  snakeBodyParts.push(new SnakeBody(headX, headY));
+  while (snakeBodyParts.length > tailLength) {
+    //remove if we have more than the tailsize
+    snakeBodyParts.shift();
+  }
 }
 
 function changeSnakePosition() {
-  //changing the headX and headY position for example if headX is 4 and the xMomentum is -1
+  //changing the headX and headY position for example if headX is 4 and the xMomentum is -1, the new position is 3
   // 4 + -1 = 3
   headX = headX + xMomentum;
   headY = headY + yMomentum;
+}
+
+function getFood() {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(foodX * tileCount, foodY * tileCount, tileSize, tileSize);
+}
+
+function checkFoodCollision() {
+  if (foodX === headX && foodY === headY) {
+    foodX = Math.floor(Math.random() * tileCount);
+    foodY = Math.floor(Math.random() * tileCount);
+    //increment the tail
+    tailLength++;
+
+  }
 }
 
 document.body.addEventListener('keydown', keyDown);
